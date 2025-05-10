1a0b8e3fb07eb98bb59f331d22f8b22bddcc7133
xian-yu-byte <1613197364@qq.com>
2025-05-10 13:57:51 +0800

    更新大量接口

---
+++ src/main/java/com/atguigu/cloud/iotcloudspring/mapper/AiDeviceMapper.java
@@ -0,0 +1,9 @@
+package com.atguigu.cloud.iotcloudspring.mapper;
+
+import com.atguigu.cloud.iotcloudspring.pojo.ai.AiDevice;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AiDeviceMapper extends BaseMapper<AiDevice> {
+}
