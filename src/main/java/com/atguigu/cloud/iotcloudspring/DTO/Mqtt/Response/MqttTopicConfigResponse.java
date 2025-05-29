c52ae69041038be0f93932ac607f50962acbe707
til <m15071452418@163.com>
2025-05-29 17:40:34 +0800

    重命名枚举

--- src/main/java/com/atguigu/cloud/iotcloudspring/DTO/Mqtt/Response/MqttTopicConfigResponse.java
+++ src/main/java/com/atguigu/cloud/iotcloudspring/DTO/Mqtt/Response/MqttTopicConfigResponse.java
@@ -1,17 +1,20 @@
 package com.atguigu.cloud.iotcloudspring.DTO.Mqtt.Response;
 
-import com.atguigu.cloud.iotcloudspring.filter.enums.mqtt.mqttTopicConfig;
+import com.atguigu.cloud.iotcloudspring.filter.enums.mqtt.MqttTopicConfigType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class MqttTopicConfigResponse {
     private Long id;
     private String fullTopic;
-    private mqttTopicConfig topicType;
-    private String autoSubscribed;
+    private MqttTopicConfigType topicType;
+    private Integer autoSubscribed;
+    private Integer effective;
     private String description;
 }
